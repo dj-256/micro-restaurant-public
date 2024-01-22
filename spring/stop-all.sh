6ac9b5b161b8220f452b4bb6560f8cb36802f86e
KilianBonnet <90778036+KilianBonnet@users.noreply.github.com>
2024-01-22 10:17:30 +0100

    Migrating to Docker Compose

--- spring/stop-all.sh
+++ spring/stop-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "stopping all"
-docker-compose --file menu-service/docker-compose-menu.yml \
+docker compose --file menu-service/docker-compose-menu.yml \
                --file dining-service/docker-compose-dining.yml \
                --file kitchen-service/docker-compose-kitchen.yml \
                --file gateway/docker-compose-gateway.yml down
